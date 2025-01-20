4c9ea90b9802e053de71e3361b1b143d04ff1a3b
USTCKevinF <fengwenjun@mail.ustc.edu.cn>
2025-01-20 09:13:31 +0800

    add system tray

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,27 +1,34 @@
 use tauri::{
-    AppHandle,
     menu::{Menu, MenuItem},
     tray::TrayIconBuilder,
+    AppHandle, Manager,
 };
+
+use crate::windows::config_window;
+use crate::windows::home_window;
 
 // 初始化托盘
 pub fn init_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
     let quit_i = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
-    let select_i = MenuItem::with_id(app, "select", "划文本", true, None::<&str>)?;
-    let screenshot_i = MenuItem::with_id(app, "screenshot", "截图选文本", true, None::<&str>)?;
-    let menu = Menu::with_items(app, &[&quit_i, &select_i, &screenshot_i])?;
-    
+    let home_i = MenuItem::with_id(app, "home", "主页", true, None::<&str>)?;
+    let config_i = MenuItem::with_id(app, "config", "配置", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&quit_i, &home_i, &config_i])?;
+
     let _tray = TrayIconBuilder::new()
         .menu(&menu)
         .icon(app.default_window_icon().unwrap().clone())
         .show_menu_on_left_click(true)
-        .on_menu_event(|app, event| match event.id.as_ref() {
+        .on_menu_event(move |app, event| match event.id.as_ref() {
             "quit" => {
-                println!("quit menu item was clicked");
                 app.exit(0);
             }
-            "translate" => {
-                // TODO: 实现翻译功能
+            "home" => {
+                println!("Home menu item clicked");
+                home_window();
+            }
+            "config" => {
+                println!("Config menu item clicked");
+                config_window();
             }
             _ => {
                 println!("menu item {:?} not handled", event.id);
