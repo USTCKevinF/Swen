91c26b6728ae7d701e5897406ee1ae37a84997c1
USTCKevinF <fengwenjun@mail.ustc.edu.cn>
2025-02-25 15:53:41 +0800

    Add internationalization to funding page

--- src/components/settings/funding.vue
+++ src/components/settings/funding.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-        <h1 class="text-sm text-center">如果觉得这个工具对你有帮助，可以请我喝杯奶茶 :)</h1>
+    <div class="h-full flex flex-col justify-center items-center">
+        <h1 class="text-sm text-center mb-4">{{ t('settings.funding.description') }}</h1>
         <img src="../../assets/images/rewardcode.jpg" class="w-80 h-80 mx-auto">
     </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
+</script>
+
+<style scoped>
 </style>
