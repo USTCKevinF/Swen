4b2bdf42fe13855b3595d7541865c0250fef64aa
USTCKevinF <fengwenjun@mail.ustc.edu.cn>
2025-03-01 21:55:36 +0800

    精简托盘菜单和注释，移除未使用的功能

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -5,36 +5,36 @@ use tauri::{
 };
 
 use crate::windows::config_window;
-use crate::windows::home_window;
-use crate::windows::system_screenshot_window;
+// use crate::windows::home_window;
+// use crate::windows::system_screenshot_window;
 
 // 初始化托盘
 pub fn init_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
     let quit_i = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
-    let home_i = MenuItem::with_id(app, "home", "主页", true, None::<&str>)?;
+    // let home_i = MenuItem::with_id(app, "home", "主页", true, None::<&str>)?;
     let config_i = MenuItem::with_id(app, "config", "配置", true, None::<&str>)?;
-    let ocr_i = MenuItem::with_id(app, "ocr", "OCR", true, None::<&str>)?;
-    let menu = Menu::with_items(app, &[&quit_i, &home_i, &config_i, &ocr_i])?;
+    // let ocr_i = MenuItem::with_id(app, "ocr", "OCR", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&config_i, &quit_i])?;
 
     let _tray = TrayIconBuilder::new()
         .menu(&menu)
         .icon(app.default_window_icon().unwrap().clone())
         .show_menu_on_left_click(true)
         .on_menu_event(move |app, event| match event.id.as_ref() {
-            "quit" => {
-                app.exit(0);
-            }
-            "home" => {
-                println!("Home menu item clicked");
-                home_window();
-            }
+            // "home" => {
+            //     println!("Home menu item clicked");
+            //     home_window();
+            // }
             "config" => {
                 println!("Config menu item clicked");
                 config_window();
             }
-            "ocr" => {
-                println!("OCR menu item clicked");
-                system_screenshot_window();
+            // "ocr" => {
+            //     println!("OCR menu item clicked");
+            //     system_screenshot_window();
+            // }
+            "quit" => {
+                app.exit(0);
             }
             _ => {
                 println!("menu item {:?} not handled", event.id);
